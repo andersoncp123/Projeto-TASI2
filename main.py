87be8ab3c9d7c90da9a8c78be3327a1eae56dee6
andersoncp123 <andersoncp321@gmail.com>
2024-11-27 19:35:38 -0300

    CSV limpo (Falta só juntar os dados por mes)

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import pandas as pd
 
-# Carregar o dataset combinado
-merged_data = pd.read_csv("covid-eua.csv")
+df = pd.read_csv('test.csv')
 
-# Preencher os valores ausentes com 0 para todas as colunas numéricas
-merged_data.fillna(0, inplace=True)
+df.rename(columns={
+    'latitude_filled': 'lat',
+    'longitude_filled': 'lng'
+}, inplace=True)
 
-# Salvar o dataset atualizado
-merged_data.to_csv("covid-eua.csv", index=False)
-
-print("Valores ausentes preenchidos com 0. Arquivo 'covid-eua.csv' atualizado com sucesso!")
+df.to_csv('covid-eua.csv', index=False)
